e21b8b5a1237628807daeb4e97f761ff28c09434
Emre <m.emrepolat@icloud.com>
2023-02-14 20:02:01 +0300

    Add few options

--- move_operations.py
+++ move_operations.py
@@ -2,6 +2,7 @@ import os
 import heapq
 import random
 from os.path import exists
+from datetime import datetime, timedelta
 
 
 def rename_files(folder_path):
@@ -51,6 +52,26 @@ def move_random_files2(src_folder, dst_folder, num_files):
     for file in random_files:
         os.rename(file, os.path.join(dst_folder, os.path.basename(file)))
 
+
+def move_by_size(src_folder, dst_folder, size_limit):  # size_limit parameter must be MegaByte
+    size_limit = size_limit * 1024 * 1024  # convert to bytes
+    for file_name in os.listdir(src_folder):
+        file_path = os.path.join(src_folder, file_name)
+        if os.path.getsize(file_path) > size_limit:
+            dst_path = os.path.join(dst_folder, file_name)
+            os.rename(file_path, dst_path)
+
+
+def move_files_by_date(src_dir, dest_dir, date):
+    target_date = datetime.strptime(date, '%Y-%m-%d')
+    for file_name in os.listdir(src_dir):
+        file_path = os.path.join(src_dir, file_name)
+        file_date = datetime.fromtimestamp(os.path.getctime(file_path))
+        # Dosya oluşturulma, belirlenen tarihten sonra ise
+        if file_date >= target_date:
+            # Dosyayı hedef klasöre taşı
+            shutil.move(file_path, dest_dir)
+
 def move_random_files(src_folder, dst_folder, num_files, file_extension=None):
     """
     Move random files from one folder to another.
@@ -79,7 +100,54 @@ def move_random_files(src_folder, dst_folder, num_files, file_extension=None):
         dst_file = os.path.join(dst_folder, file)
         os.rename(src_file, dst_file)
 
+
+
+def without_extension(_fileName):
+    without_ext = os.path.splitext(_fileName)[0]
+    return without_ext
+
+
+def get_extension(_fileName):
+    ext = os.path.splitext(_fileName)[1]
+    return ext
+
+
+def move_common_files(src1, src2, dst):
+    src1_files = os.listdir(src1)
+    print(src1_files)
+    src1_files_wo_ext = []
+    for x in range(0, len(src1_files)):
+        src1_files_wo_ext.append(without_extension(src1_files[x]))
+    print(src1_files_wo_ext)
+
+    src2_files_wo_ext = []
+    src2_files = os.listdir(src2)
+    for y in range(0, len(src2_files)):
+        src2_files_wo_ext.append(without_extension(src2_files[y]))
+    print(src2_files_wo_ext)
+    common_files = set(src1_files_wo_ext) & set(src2_files_wo_ext)
+    print(len(common_files))
+    for x in range(0, len(common_files)):
+        src1_path = os.path.join(src1, file_name)
+        print(src1_path)
+        src2_path = os.path.join(src2, file_name)
+        print(src2_path)
+        dst_path = os.path.join(dst, file_name)
+        print(dst_path)
+        src1_ext = os.path.splitext(src1_path)[1]
+        src2_ext = os.path.splitext(src2_path)[1]
+        if src1_ext != src2_ext:
+           # os.rename(src1_path, dst_path)
+            os.rename(src2_path, dst_path)
+
+#move_common_files('Person1', 'Person2', 'Person3')
 # change_extension(input("File Path:"), input("Enter the extensions that need to be changed:"), input("Enter the extension to be changed:"))
 # move_largest_files('Person2','Person1' , 15)
 # move_random_files('Person1', 'Person2', 20)
-#special_rename_files('Person3')
+# special_rename_files('Person3')
+# move_files_by_date(src_dir, dest_dir, "2022-01-01")
+# move_by_size(src_folder, dst_folder, 100)
+
+
+
+
